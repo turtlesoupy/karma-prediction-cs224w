9b9289b37fd83bc5141dce11789565a5b32a9c7c
Thomas Dimson <tdimson@gmail.com>
2013-10-19 22:17:29 -0700

    Initial commit

---
+++ src/cs224w_karma_prediction/core.clj
@@ -0,0 +1,6 @@
+(ns cs224w-karma-prediction.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/cs224w_karma_prediction/core_test.clj
@@ -0,0 +1,7 @@
+(ns cs224w-karma-prediction.core-test
+  (:require [clojure.test :refer :all]
+            [cs224w-karma-prediction.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
