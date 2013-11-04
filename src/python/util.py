a097406a317eee3831aea5f36ad73cc3772ae541
Thomas Dimson <tdimson@gmail.com>
2013-11-03 21:28:21 -0800

    Plot CCDF function too, add relative paths

--- src/python/util.py
+++ src/python/util.py
@@ -1,5 +1,21 @@
-def xs(seq):
-    return [e[0] for e in seq]
+import numpy as np
 
-def ys(seq):
-    return [e[1] for e in seq]
+def xs(seq): return list(ixs(seq))
+def ixs(seq): return (e[0] for e in seq)
+def ys(seq): return list(iys(seq))
+def iys(seq): return (e[1] for e in seq)
+
+def mkccdf(ys):
+    return [1 - e for e in mkcdf(ys)]
+
+def mkcdf(ys):
+    if ys == []:
+        return []
+
+    y = np.cumsum(ys)
+    return [float(e) / y[-1] for e in y]
+
+
+def mkpdf(ys):
+    s = sum(ys)
+    return [float(e) / s for e in ys]
