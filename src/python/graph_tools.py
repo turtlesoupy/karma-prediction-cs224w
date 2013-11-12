0b49e50d131ee062e78be176a23a5948f8c4b177
Thomas Dimson <tdimson@gmail.com>
2013-11-11 23:29:28 -0800

    Fix graph tools

--- src/python/graph_tools.py
+++ src/python/graph_tools.py
@@ -16,12 +16,12 @@ def compute_network_stats(graph):
     num_nodes = graph.number_of_nodes()
     num_edges = graph.number_of_edges()
 
-    strongly_connected_subgraphs = nx.algorithms.components.strongly_connected_component_subgraphs(graph)
-    largest_strongly_connected = float(len(strongly_connected_subgraphs[0].number_of_nodes())) / graph.number_of_nodes()
+    strongly_connected = nx.algorithms.components.strongly_connected_components(graph)
+    largest_strongly_connected = float(len(strongly_connected[0])) / graph.number_of_nodes()
     weakly_connected = nx.algorithms.components.weakly_connected_components(graph)
     largest_weakly_connected = float(len(weakly_connected[0])) / graph.number_of_nodes()
 
-    average_karma = np.mean(x[1].get("karma", x[1].get("reputation", 0)))
+    average_karma = np.mean([x[1].get("karma", x[1].get("reputation", 0)) for x in graph.nodes(data=True)])
 
     return {
         "Nodes": num_nodes,
@@ -33,9 +33,10 @@ def compute_network_stats(graph):
 
 def network_stats_table(*dicts):
     ret = [r"\begin{tabular}{r | %s}" % " ".join("c" for c in dicts)]
-    ret.append("& Graph %d" % i for i,e in enumerate(dicts))
-    for field in dicts[0].iterkeys():
-        ret.append("%s & %s" % (field, " & ".join(d[field] for d in dicts)))
+    ret.append(" ".join("& Graph %d" % i for i,e in enumerate(dicts)))
+    ret.append(r"\hline")
+    for field in sorted(dicts[0].iterkeys()):
+        ret.append("%s & %s" % (field, " & ".join(str(d[field]) for d in dicts)))
     ret.append(r"\end{tabular}")
 
     return "\n".join(ret)
