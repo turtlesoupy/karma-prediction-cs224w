ea125800740d636e3f3dad4f49489188996de514
Milind Ganjoo <mganjoo@stanford.edu>
2013-12-10 15:07:22 -0800

    Update report draft

--- scripts/stats_csv_to_pickle.py
+++ scripts/stats_csv_to_pickle.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 
-import pickle
+import cPickle as pickle
 import csv
 
 ID_KEY = "#NodeId"
 CONSTRAINT_KEY = "NetworkConstraint"
+CLOSENESS_KEY = "Closeness"
+BETWEENNESS_KEY = "Betweennes"
 
-def read_constraint_into_dict(filename):
+def read_dicts(filename):
     constraint_dict = {}
+    closeness_dict = {}
+    betweeness_dict = {}
     with open(filename, "r") as f:
         # Skip first two lines
         for i in range(2): f.next()
         csv_reader = csv.DictReader(f, delimiter="\t")
         for row in csv_reader:
             constraint_dict[int(row[ID_KEY])] = float(row[CONSTRAINT_KEY])
-    return constraint_dict
+            closeness_dict[int(row[ID_KEY])] = float(row[CLOSENESS_KEY])
+            betweeness_dict[int(row[ID_KEY])] = float(row[BETWEENNESS_KEY])
+
+    return constraint_dict, closeness_dict, betweeness_dict
 
 def main():
-    su_constraint_dict = read_constraint_into_dict("../data/su_stats.txt")
-    pickle.dump(su_constraint_dict, open("../data/su_constraint.p", "wb"))
-
+    constraint, closeness, betweeness = read_dicts("su_stats.txt")
+    pickle.dump(constraint, open("su_constraint.pickle", "wb"), -1) 
+    pickle.dump(closeness, open("su_closeness.pickle", "wb"), -1) 
+    pickle.dump(betweeness, open("su_betweenness.pickle", "wb"), -1) 
+    
 if __name__ == '__main__':
     main()
